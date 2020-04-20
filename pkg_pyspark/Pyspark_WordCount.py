a5d8d74f4a254304ac4eaf03baa11736b17198f1
pradeep-jha <pradeep.jha1987@gmail.com>
2020-04-20 21:31:37 +0530

    Commit after Word count

---
+++ pkg_pyspark/Pyspark_WordCount.py
@@ -0,0 +1,14 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from pyspark.conf import SparkConf
+sc = SparkContext(master="local",appName="Spark Demo")
+inp=sc.textFile("C:\\Users\\pradeep\\Desktop\pycharm_spark\\input\\Titanic_header.txt")
+SparkConf().getAll()
+'''spark = SparkSession \
+            .builder \
+            .appName("wordcount") \
+            .master("local[*]") \
+            .getOrCreate()
+inp=spark.read.text("C:\\Users\\pradeep\\Desktop\pycharm_spark\\input\\Titanic.txt")'''
+words=inp.flatMap(lambda x: x.split('\t')).map(lambda x: (x,1)).reduceByKey(lambda x,y: x+y)
+words.foreach(print)
