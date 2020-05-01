bdd6546a8e051fe47d5ebdd45871e60c982d907f
pradeep-jha <pradeep.jha1987@gmail.com>
2020-05-01 20:13:39 +0530

    Commit after Word count and includes .egg file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='First_Project_pyspark',
+    version='1.0',
+    packages=['pkg_pyspark'],
+    url='',
+    license=
+    author='pradeep',
+    author_email='',
+    description=''
+)
