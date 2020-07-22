b83e45ee25f04486c93b7a1dfbc434a44d11567a
pradeep-jha <pradeep.jha1987@gmail.com>
2020-07-23 01:38:24 +0530

    code to send notifiation for new device added to WiFi network

---
+++ proj_python_core/Network_Communicator.py
@@ -0,0 +1,32 @@
+import sys
+import subprocess
+from win32com.client import Dispatch
+import os
+#from decouple import config
+
+def speak(str):
+    speak = Dispatch("SAPI.spVoice")
+    speak.Speak(str)
+
+IP_NETWORK = '192.168.0.103'
+IP_DEVICE = '192.168.0.103'
+proc = subprocess.Popen(["ping", IP_NETWORK],stdout=subprocess.PIPE)
+print(proc.pid)
+line = proc.stdout.readline()
+print(line.split())
+print(proc.stdout.readline())
+#
+#print(proc.stdout.readline().split(''))
+while True:
+  line = proc.stdout.readline()
+  print(line)
+  if not line:
+    break
+  #the real code does filtering here
+  #print(line.decode('utf-8').lower())
+  connected_ip = line.decode('utf-8').__contains__("Reply")
+  #print(connected_ip)
+  #print(IP_DEVICE)
+  if connected_ip :
+	  speak("pradeep mobile just connected to the network")
+	  #subprocess.Popen(["say", "Linnea just connected to the network"])
