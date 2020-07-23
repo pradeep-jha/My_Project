adbba6e45720bc952eda33ed5a22a889f24dd76a
pradeep-jha <pradeep.jha1987@gmail.com>
2020-07-23 22:45:42 +0530

    Jarvis chat boat code

--- proj_python_core/Jarvis_Chatboat.py
+++ proj_python_core/Jarvis_Chatboat.py
@@ -30,29 +30,40 @@ def takeCommand():
         return "None"
     return query
 if __name__=="__main__":
-    audio=takeCommand()
-    if "time" in str.lower(audio):
-        time_12 = time.strftime("%I:%M %p")
-        #time_12_h_m_s = time.strftime("%I:%M:%S %p")
-        print(time_12)
-        speak(f"The current time is: {time_12}")
-        audio = takeCommand()
-        #speak(time_12)
-    elif "music" in str.lower(audio):
-        music_dir="E:\songs"
-        songs=os.listdir(music_dir)
-        print(songs)
-        print(songs[10])
-        os.startfile(os.path.join(music_dir,songs[10]))
-        time.sleep(10)
-        audio = takeCommand()
-    elif "google" in str.lower(audio):
-        webbrowser.open("google.com")
-        time.sleep(10)
-        audio = takeCommand()
-    elif "youtube news" in str.lower(audio):
-            speak("opening republic news in youtube")
-            webbrowser.open("https://www.youtube.com/watch?v=ckKA-0MUm4E")
-            time.sleep(10)
-            audio = takeCommand()
+    while True:
+        audio=takeCommand()
+        if "time" in str.lower(audio):
+            time_12 = time.strftime("%I:%M %p")
+            #time_12_h_m_s = time.strftime("%I:%M:%S %p")
+            print(time_12)
+            speak(f"The current time is: {time_12}")
+            #audio = takeCommand()
+            #speak(time_12)
+        elif "music" in str.lower(audio):
+            music_dir="E:\songs"
+            songs=os.listdir(music_dir)
+            print(songs)
+            print(songs[10])
+            speak("opening Music from local folder")
+            os.startfile(os.path.join(music_dir,songs[10]))
+            # time.sleep(10)
+            # audio = takeCommand()
+        elif "google" in str.lower(audio):
+            speak("opening google in internet explorer!")
+            webbrowser.open("google.com")
+            # time.sleep(10)
+            # audio = takeCommand()
+        elif "youtube news" in str.lower(audio):
+                speak("opening republic news in youtube")
+                webbrowser.open("https://www.youtube.com/watch?v=ckKA-0MUm4E")
+                # time.sleep(10)
+                # audio = takeCommand()
+        elif "notepad" in str.lower(audio):
+                speak("opening Notepad")
+                webbrowser.open("C:\\Users\\pradeep\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Accessories\\Notepad")
+        elif "news" in str.lower(audio):
+                os.startfile("C:\\Users\\pradeep\\Desktop\\News_Reader.exe")
 
+        elif "stop" in str.lower(audio):
+                speak("JARVIS shutting down!")
+                exit()
