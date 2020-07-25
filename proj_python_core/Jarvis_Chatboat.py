d25b8515bff69aee6ce138cfddbfa9ec5bbcd631
pradeep-jha <pradeep.jha1987@gmail.com>
2020-07-26 02:18:51 +0530

    Created temprature calculator code and added the same in Jarvis code.

--- proj_python_core/Jarvis_Chatboat.py
+++ proj_python_core/Jarvis_Chatboat.py
@@ -6,7 +6,8 @@ import datetime
 import time
 import os
 import webbrowser
-
+import random
+from proj_python_core import Teprature_calculaor
 
 # from decouple import config
 
@@ -26,28 +27,55 @@ def takeCommand():
         print(f"user said :{query}\n")
     except Exception as e:
         print(e)
-        print("Say that again please..")
+        print("I am sorry! Say that again please..")
+        #speak("I am sorry! Say that again please..")
         return "None"
     return query
 if __name__=="__main__":
     while True:
+        speak("Hi There! How may I help you")
         audio=takeCommand()
         if "time" in str.lower(audio):
             time_12 = time.strftime("%I:%M %p")
             #time_12_h_m_s = time.strftime("%I:%M:%S %p")
             print(time_12)
             speak(f"The current time is: {time_12}")
-            #audio = takeCommand()
-            #speak(time_12)
-        elif "music" in str.lower(audio):
+        elif "audio" in str.lower(audio):
             music_dir="E:\songs"
             songs=os.listdir(music_dir)
-            print(songs)
-            print(songs[10])
-            speak("opening Music from local folder")
-            os.startfile(os.path.join(music_dir,songs[10]))
-            # time.sleep(10)
-            # audio = takeCommand()
+            #print(songs)
+            audio_songs = []
+            for song in songs:
+                if (song[len(song) - 3:] == "mp3"):
+                    audio_songs.append(song)
+            print(audio_songs)
+            n = random.randint(0, len(audio_songs)-1)
+            print("song number: ",n)
+            song_name_noext = audio_songs[n][:len(audio_songs[n])-4]
+            print(song_name_noext)
+            song_name=audio_songs[n]
+            print(song_name)
+            speak(f"opening the audio song {song_name_noext}  from local folder")
+            os.startfile(os.path.join(music_dir, song_name))
+
+        elif "video" in str.lower(audio):
+            music_dir="E:\songs"
+            songs=os.listdir(music_dir)
+            #print(songs)
+            vedio_songs = []
+            for song in songs:
+                if (song[len(song) - 3:] == "mp4"):
+                    vedio_songs.append(song)
+            print(vedio_songs)
+            n = random.randint(0, len(vedio_songs)-1)
+            print("song number: ", n)
+            song_name_noext = vedio_songs[n][:len(vedio_songs[n]) - 4]
+            print(song_name_noext)
+            song_name = vedio_songs[n]
+            print(song_name)
+            speak(f"opening the video song {song_name_noext}  from local folder")
+            os.startfile(os.path.join(music_dir, song_name))
+
         elif "google" in str.lower(audio):
             speak("opening google in internet explorer!")
             webbrowser.open("google.com")
@@ -56,14 +84,56 @@ if __name__=="__main__":
         elif "youtube news" in str.lower(audio):
                 speak("opening republic news in youtube")
                 webbrowser.open("https://www.youtube.com/watch?v=ckKA-0MUm4E")
-                # time.sleep(10)
-                # audio = takeCommand()
+        elif "playlist" in str.lower(audio):
+                speak("opening  songs from your playlist in youtube")
+                webbrowser.open("https://www.youtube.com/watch?v=m4wzXcfbkOQ&list=PLGsuPLNadTJLEOByd1rQs2mG9AUIH9Jj9")
         elif "notepad" in str.lower(audio):
-                speak("opening Notepad")
-                webbrowser.open("C:\\Users\\pradeep\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Accessories\\Notepad")
-        elif "news" in str.lower(audio):
-                os.startfile("C:\\Users\\pradeep\\Desktop\\News_Reader.exe")
+                speak("opening a new Notepad")
+                os.startfile("C:\\Users\\pradeep\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Accessories\\Notepad")
+        elif "excel" in str.lower(audio):
+                speak("opening a new excel spreadsheet")
+                os.startfile("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Microsoft Office 2013\\Excel 2013")
+        elif "quick note" in str.lower(audio):
+            speak("opening a new quick note")
+            os.startfile("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Microsoft Office 2013\\Excel 2013")
 
+        elif "ppt" in str.lower(audio):
+                speak("opening a new Power point presentation")
+                os.startfile("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Microsoft Office 2013\\PowerPoint 2013")
+        elif "word" in str.lower(audio):
+                speak("opening a new MS word file")
+                os.startfile("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Microsoft Office 2013\\Word 2013")
+        elif "presentation" in str.lower(audio):
+                speak("opening hadoop presentation")
+                os.startfile("C:\\Users\\pradeep\\Desktop\\python_autmation_codes\\ppt_sample\\Hadoop.pptx")
+        elif "headlines" in str.lower(audio):
+                os.startfile("C:\\Users\\pradeep\\Desktop\\News_Reader.exe")
+        elif "weather" in str.lower(audio):
+                speak("please tell the city name")
+                city = takeCommand()
+                city_name = str.lower(city)
+                if city_name !='none':
+                    print("city name is :",city_name)
+                    try:
+                        weather = Teprature_calculaor.get_weather(city_name)
+                        speak(weather)
+                    except Exception as e:
+                        #speak("got am exception")
+                        #print(e)
+                        speak(f"{city_name} is not a correct city name")
+                        speak("please tell the correct city name")
+                        try:
+                            city = takeCommand()
+                            city_name = str.lower(city)
+                            if city_name != 'none':
+                                print("city name is :", city_name)
+                                weather = Teprature_calculaor.get_weather(city_name)
+                                speak(weather)
+                        except Exception as e:
+                            speak("No correct city name found! Taking you to main menu...")
+                            speak("........")
+                            #audio = takeCommand()
+                        # speak("I am sorry! Say that again please..")
         elif "stop" in str.lower(audio):
-                speak("JARVIS shutting down!")
+                speak("THANK YOU ! JARVIS shutting down!")
                 exit()
---
+++ proj_python_core/Teprature_calculaor.py
@@ -0,0 +1,31 @@
+import requests
+from pprint import pprint
+#city_name=input("Enter city name:")
+def get_weather(city):
+
+    api_key="8b683c130179b9ccf36437390ed5fd1b"
+    url=f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+    #  "&units=metric" --for geting the tem in degree celcious
+    #print(url)
+    res=requests.get(url)
+
+    #print(res.text)
+    json_data=res.json()
+    pprint(json_data) # pprint for more readable json"
+    city=json_data['name']
+    temp=json_data['main']['temp']
+    weather=json_data['weather'][0]['description']
+    wind_speed=json_data['wind']['speed']
+    latitude=json_data['coord']['lat']
+    longitude=json_data['coord']['lon']
+
+    print("temp= ",temp)
+    print("weather= ",weather)
+    print("wind_speed= ",wind_speed)
+    print("latitude= ",latitude)
+    print("longitude= ",longitude)
+    weather_details=f" The temprature of {city} is {temp} degree celsius and the weather is {weather}"
+    return weather_details
+
+#weather=get_weather(city_name)
+#print(weather)
