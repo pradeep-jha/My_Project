df8ccfe3d8c1613157f35ff6afce32481024033c
PRADEEP <pr>
2022-07-13 17:20:15 +0530

    updated code on 13-07-2022

--- proj_python_core/Teprature_calculaor.py
+++ proj_python_core/Teprature_calculaor.py
@@ -11,7 +11,7 @@ def get_weather(city):
     #api_key="8b683c130179b9ccf36437390ed5fd1b"
     url=f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
     #  "&units=metric" --for geting the tem in degree celcious
-    #print(url)
+    print(url)
     res=requests.get(url)
 
     #print(res.text)
@@ -32,5 +32,5 @@ def get_weather(city):
     weather_details=f" The temprature of {city} is {temp} degree celsius and the weather is {weather}"
     return weather_details
 
-#weather=get_weather(city_name)
+#weather=get_weather('Bangalore')
 #print(weather)
